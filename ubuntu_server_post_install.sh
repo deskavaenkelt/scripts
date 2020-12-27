00c78a224b7afd86c8fead4fc08ba53b95f4ab88
Lars Strömberg <43166006+deskavaenkelt@users.noreply.github.com>
2020-12-27 23:44:30 +0100

    Update ubuntu_server_post_install.sh

--- ubuntu_server_post_install.sh
+++ ubuntu_server_post_install.sh
@@ -3,4 +3,6 @@
 echo Lars Ubuntu update script
 
 # Update all
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt autoremove && sudo apt-get autoclean
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt autoremove
+sudo apt-get autoclean
